436ce8382bcc1fd75959cd92b7a6ae3ebdcf224d
Christopher Night <cosmologicon@gmail.com>
2012-02-06 23:41:21 -0500

    Pause module Scene test that auto-pauses after 1 second

---
+++ UFX/src/pause.js
@@ -0,0 +1,56 @@
+// The pause scene - click to continue
+// Requires modules scene and mouse
+// Draws to the global var context
+
+
+UFX.pause = function () {
+    UFX.scene.push(UFX.pause.Pause)
+}
+UFX.pause.unpause = function () {
+    if (UFX.pause.ispaused()) {
+        UFX.scene.pop()
+    }
+}
+UFX.pause.ispaused = function () {
+    return UFX.scene.top() === UFX.pause.Pause
+}
+
+UFX.pause.Pause = Object.create(UFX.scene.Scene)
+UFX.pause.Pause.start = function () {
+    var c0 = context.canvas
+    this.x = c0.width
+    this.y = c0.height
+    this.backdrop = document.createElement("canvas")
+    this.backdrop.width = this.x
+    this.backdrop.height = this.y
+    this.bcontext = this.backdrop.getContext("2d")
+    this.bcontext.drawImage(c0, 0, 0)
+    this.bcontext.fillStyle = "rgba(64,64,64,0.7)"
+    this.bcontext.fillRect(0, 0, this.x, this.y)
+    this.t = 0
+    this.r = Math.min(this.x, this.y) * 0.3
+}
+
+UFX.pause.Pause.think = function (dt) {
+    context.drawImage(this.backdrop, 0, 0)
+    this.t += dt
+
+    context.save()
+    context.translate(0.5*this.x, 0.5*this.y)
+    if (this.t < 0.25) {
+        var theta = 30 * this.t
+        var r = 1 - 4 * this.t
+        context.scale(1. + r * Math.cos(theta), 1. - r * Math.sin(theta))
+    }
+    context.scale(this.r, this.r)
+    context.fillStyle = "rgba(255,255,255,0.5)"
+    context.beginPath()
+    context.moveTo(0.8, 0)
+    context.lineTo(-0.5, -0.7)
+    context.lineTo(-0.5, 0.7)
+    context.closePath()
+    context.fill()
+    context.restore()
+}
+
+
--- UFX/src/scene.js
+++ UFX/src/scene.js
@@ -3,39 +3,59 @@
 if (typeof UFX == "undefined") UFX = {}
 UFX.scene = {}
 
+UFX.scene.init = function (maxups, minups) {
+    maxups = maxups || 300
+    minups = minups || 10
+    UFX.ticker.registersync(UFX.scene.think, UFX.scene.draw, maxups, minups)
+}
+
 UFX.scene.top = function () {
     var n = UFX.scene._stack.length
     return n ? UFX.scene._stack[n-1] : null
 }
 UFX.scene.push = function (c) {
+    var old = UFX.scene.top()
+    if (old) old.suspend()
     UFX.scene._stack.push(c)
+    c.start()
 }
 UFX.scene.pop = function () {
     var c = UFX.scene._stack.pop()
+    c.stop()
+    var n = UFX.scene.top()
+    if (n) n.resume()
     return c
 }
 UFX.scene.think = function (dt) {
     var c = UFX.scene.top()
-    var args = c.thinkargs(dt)
-    c.think.apply(c, args)
     UFX.scene._lastthinker = c
+    if (c) {
+        var args = c.thinkargs(dt)
+        c.think.apply(c, args)
+    }
 }
 UFX.scene.draw = function (f) {
-    UFX.scene._lastthinker.draw(f)
+    if (UFX.scene._lastthinker) {
+        UFX.scene._lastthinker.draw(f)
+    }
 }
 
-
-UFX.scene._stack = []
-
+// Use this as a prototype for your own scenes
 UFX.scene.Scene = {
     thinkargs: function (dt) {
         return [dt]
     },
     think: function (dt) {
-    }    
+    },
     draw: function (f) {
-    }
+    },
+    start: function () { }, // called when the scene is pushed onto the stack
+    suspend: function () { }, // called when another scene is pushed onto this one
+    resume: function () { },  // called when another scene is popped off of this one
+    stop: function () { }, // called when this scene is popped off the stack
 }
+
+UFX.scene._stack = []
 
 
 
