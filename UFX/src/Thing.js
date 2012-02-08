4eb3fa565b43b179de8b1208ce6a7dcb5d2d0244
Christopher Night <cosmologicon@gmail.com>
2012-02-08 00:28:29 -0500

    Created prototype object for UFX.Thing

--- UFX/src/Thing.js
+++ UFX/src/Thing.js
@@ -2,65 +2,68 @@
 
 if (typeof UFX == "undefined") UFX = {}
 
-// Thing constructor. Give it a Component or a list of Components
+// Thing factory/constructor. Give it a Component or a list of Components
 UFX.Thing = function (comps) {
-    var thing = {
-        _comps: [],
-        definemethod: function (mname) {
-            if (this[mname]) return
-            var mlist = []
-            this[mname] = function () {
-                var ret
-                for (var j = 0 ; j < mlist.length ; ++j) {
-                    ret = mlist[j].apply(this, arguments)
-                }
-                return ret
-            }
-            this[mname].mlist = mlist
-            return this
-        },
-        addcomp: function (comp) {
-            if (comp instanceof Array) {
-                for (var j = 0 ; j < comp.length ; ++j) {
-                    this.addcomp(comp[j])
-                }
-                return this
-            }
-            for (var mname in comp) {
-                if (mname == "init" || mname == "remove") continue
-                if (typeof comp[mname] != "function") continue
-                this.definemethod(mname)
-                this[mname].mlist.push(comp[mname])
-            }
-            this._comps.push(comp)
-            if (comp.init) {
-                comp.init.apply(this, [].slice.call(arguments, 1))
-            }
-            return this
-        },
-        removecomp: function (comp) {
-            if (comp instanceof Array) {
-                for (var j = 0 ; j < comp.length ; ++j) {
-                    this.removecomp(comp[j])
-                }
-                return
-            }
-            if (comp.remove) {
-                comp.remove.apply(this, [].slice.call(arguments, 1))
-            }
-            for (var mname in comp) {
-                if (mname == "init" || mname == "remove") continue
-                if (!(mname in this)) continue
-                this[mname].mlist = this[mname].mlist.filter(function (m) { return m !== comp[mname] })
-            }
-            this._comps.push(comp)
-        },
-    }
+    var thing = Object.create(UFX.Thing.prototype)
+    thing._comps = []
     if (comps) {
         thing.addcomp(comps)
     }
     return thing
 }
+UFX.Thing.prototype = {
+    definemethod: function (mname) {
+        if (this[mname]) return
+        var mlist = []
+        this[mname] = function () {
+            var ret
+            for (var j = 0 ; j < mlist.length ; ++j) {
+                ret = mlist[j].apply(this, arguments)
+            }
+            return ret
+        }
+        this[mname].mlist = mlist
+        return this
+    },
+    addcomp: function (comp) {
+        if (comp instanceof Array) {
+            for (var j = 0 ; j < comp.length ; ++j) {
+                this.addcomp(comp[j])
+            }
+            return this
+        }
+        for (var mname in comp) {
+            if (mname == "init" || mname == "remove") continue
+            if (typeof comp[mname] != "function") continue
+            this.definemethod(mname)
+            this[mname].mlist.push(comp[mname])
+        }
+        this._comps.push(comp)
+        if (comp.init) {
+            comp.init.apply(this, [].slice.call(arguments, 1))
+        }
+        return this
+    },
+    removecomp: function (comp) {
+        if (comp instanceof Array) {
+            for (var j = 0 ; j < comp.length ; ++j) {
+                this.removecomp(comp[j])
+            }
+            return
+        }
+        if (comp.remove) {
+            comp.remove.apply(this, [].slice.call(arguments, 1))
+        }
+        for (var mname in comp) {
+            if (mname == "init" || mname == "remove") continue
+            if (!(mname in this)) continue
+            this[mname].mlist = this[mname].mlist.filter(function (m) { return m !== comp[mname] })
+        }
+        this._comps.push(comp)
+    },
+}
+
+
 
 UFX.Component = {}
 
