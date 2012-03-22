8c68db0045fe97622ffcf3e519b9fb40cd672d34
Christopher Night <cosmologicon@gmail.com>
2012-03-21 21:28:27 -0400

    Abigail's Descent settings module

--- UFX/test/abigail/play.js
+++ UFX/test/abigail/play.js
@@ -6,26 +6,30 @@ var PlayScene = {
         this.tower = Tower(500, [128, 128, 128])
 //        var p = Portal(this.tower, [100, 100], this.tower, [-20, 10])
 //        var p = Platform(this.tower, -40, 100, 100)
-        for (var h = 36 ; h < 1600 ; h += 24) {
+        for (var h = 36 ; h < 1600 ; h += 40) {
             var x0 = this.tower.circ * Math.random(), dx = Math.random() * 80 + 40
             var p = Platform(this.tower, x0, x0 + dx, h)
         }
         var ground = Ground(this.tower)
         this.you = You(this.tower, [0, 600])
-        UFX.key.watchlist = "up down left right".split(" ")
+        UFX.key.watchlist = "up down left right esc P".split(" ")
     },
 
-    think: function (dt) {
-        this.tower.think(dt)
-        var you = this.you
+    thinkargs: function (dt) {
+        var press = {}
         UFX.key.events().forEach(function (event) {
             if (event.type === "down") {
-                if (event.name === "up") {
-                    you.jump()
-                }
+                press[event.name] = true
             }
         })
         var keymove = (UFX.key.ispressed.right ? 1 : 0) - (UFX.key.ispressed.left ? 1 : 0)
+        return [dt, press, keymove]
+    },
+
+    think: function (dt, press, keymove) {
+        this.tower.think(dt)
+        if (press.up) this.you.jump()
+        if (press.esc || press.P) UFX.pause()
         this.you.step(dt * keymove)
         var lookpos = this.you.lookingat()
         this.tower.panto(lookpos[0], lookpos[1])
@@ -34,15 +38,10 @@ var PlayScene = {
     
     draw: function () {
         context.fillStyle = "rgba(0,0,128,1)"
-        context.fillRect(0, 0, 540, 540)
+        context.fillRect(0, 0, settings.canvaswidth, settings.canvasheight)
         context.save()
-        context.translate(270, 270)
-//        var s = 1 + 0.2 * Math.sin(Date.now() * 0.001 / 4)
-//        var s = 2
-//        context.scale(s, s)
-//        context.rotate(2 * this.tower.y0 / 800)
-//        this.tower.draw([-380/s, 380/s])
-        this.tower.draw([-270, 270])
+        context.translate(settings.canvaswidth/2, settings.canvasheight/2)
+        this.tower.draw([-settings.canvasheight/2, settings.canvasheight/2])
         context.restore()
     },
 }
---
+++ UFX/test/abigail/settings.js
@@ -0,0 +1,11 @@
+settings = {
+    dramatictilt: false,
+    walkspeed: 200,
+    jumpspeed: 320,
+    
+    canvaswidth: 540,
+    canvasheight: 640,
+    
+    
+}
+
