362507b94f2c3aa1d00822ba6758468fae100bf2
Christopher Night <cosmologicon@gmail.com>
2012-04-15 01:46:37 -0400

    Lepidopterist Add mechanics global Minor player state refactor

--- lepidopterist/settings.js
+++ lepidopterist/settings.js
@@ -1,5 +1,6 @@
 // Convenient global variable containing settings
 
+// Options like screen resolution (ie, things that the player might reasonably change)
 var settings = {
     // Canvas size
     sx: 800,
@@ -7,5 +8,12 @@ var settings = {
     // Swap left/right in combos when facing left
     swapcontrols: true,
 }
+
+// Game mechanics (players shouldn't generally change these)
+var mechanics = {
+    runvx: 200,  // vx on the ground
+    turnvx: 0, turnvy: 200,
+}
+
 
 
