7c30c14c5abdbe74d9fd71258e9aa5d4eee76640
Christopher Night <cosmologicon@gmail.com>
2012-04-20 23:19:22 -0400

    LD23: Jumping physics and player states

--- ld23/settings.js
+++ ld23/settings.js
@@ -1,12 +1,19 @@
 
 
 var settings = {
-    sx: 854,
-    sy: 480,
+    sx: 600,
+    sy: 600,
 }
 
 var mechanics = {
     runspeed: 120,
+    jumphspeed: 120,
+    launchspeed: 140,
+    launchaccel: 2000,
+    
+    gravity: 800,  // non-resisted gravity
+    rgravity: 400,  // resisted gravity
+    
     lookahead: 10000,
 
 }
