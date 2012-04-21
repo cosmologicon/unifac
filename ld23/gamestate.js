b56f9f1fa596a37a2250198be62e6dd5b24d009b
Christopher Night <cosmologicon@gmail.com>
2012-04-21 12:01:02 -0400

    LD23 Build slot indicator

--- ld23/gamestate.js
+++ ld23/gamestate.js
@@ -1,14 +1,30 @@
 
-var gamestate = {
-    worldsize: 400,
+gamestate = Object.create({
+    setworldsize: function (s) {
+        this.worldsize = s
+        this.worldr = s / (2 * Math.PI)
+        this.nslots = Math.floor(s / 50)
+    },
+    // If player is at x, where's the nearest building location?
+    buildindex: function(x) {
+        var n = Math.floor(x / tau * this.nslots + 0.5)
+        return ((n % this.nslots) + this.nslots) % this.nslots
+    },
 
-    // Abilities
-    njumps: 2,  // How many jumps can you perform
-    
-    
-    bank: 100,
-    hp: 100,
-}
+    buildat: function (x) {
+        return this.buildindex(x) * tau / this.nslots
+    },
+})
+
+// Abilities
+gamestate.njumps = 2  // How many jumps can you perform
+gamestate.bank = 100
+gamestate.hp = 100
+
+
+gamestate.setworldsize(400)
+
+
 
 
 function upgrade(button) {
@@ -22,8 +38,7 @@ function upgrade(button) {
         gamestate.bank -= 10
     }
     if (button.id === "upgradeworld") {
-        gamestate.worldsize += 100
-        gamestate.worldr = gamestate.worldsize / tau
+        gamestate.setworldsize(gamestate.worldsize + 100)
     }
 }
 
