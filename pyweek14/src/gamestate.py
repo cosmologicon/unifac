bf6c5cd8d9f1055fcc91d65067cd00db681130e7
Christopher Night <cosmologicon@gmail.com>
2012-05-06 13:44:43 -0400

    PyWeek 14 - can bind mouse buttons to techs

--- pyweek14/src/gamestate.py
+++ pyweek14/src/gamestate.py
@@ -8,9 +8,18 @@ def save():
 def load():
     pass
 
+def bind(objname, key):
+    global rbindings
+    if objname in bindings and bindings[objname] == key:
+        del bindings[objname]
+    else:
+        if key in rbindings and rbindings[key] in bindings:
+            del bindings[rbindings[key]]
+        bindings[objname] = key
+    rbindings = dict((o,k) for k,o in bindings.items())
 
 def loadlevel():
-    global elements, inventions, map0
+    global elements, inventions, map0, bindings, rbindings
 
     # available (researched) elements and inventions
     elements = list(mechanics.elements)
@@ -18,5 +27,7 @@ def loadlevel():
     
     # initial map (with nothing built on it)
     map0 = pygame.image.load(data.filename("map-0.png"))
+    bindings = {}
+    rbindings = {}
 
 
