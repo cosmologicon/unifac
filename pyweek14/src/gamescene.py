4b759ceb005a571c40131b11940bc1e7560e4c90
Christopher Night <cosmologicon@gmail.com>
2012-05-05 23:30:02 -0400

    PyWeek14: swap between modes in game context

--- pyweek14/src/gamescene.py
+++ pyweek14/src/gamescene.py
@@ -1,14 +1,20 @@
-import vista
+import pygame
+import vista, gamemap, tech
 
 class GameScene():
     def __init__(self):
         pass
     
     def think(self, dt, events):
-        pass
+        vista.think(dt)
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_TAB:
+                vista.swapmode()
 
     def draw(self):
-        vista.clear()
+        gamemap.draw()
+        tech.draw()
+        vista.drawwindows()
         
         vista.flip()
 
