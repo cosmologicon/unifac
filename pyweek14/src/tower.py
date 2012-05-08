f3e39d04cdaea7a2c54a6084365db2ce8aa07000
Christopher Night <cosmologicon@gmail.com>
2012-05-07 21:49:23 -0400

    Tower graphics changes colors with elements

--- pyweek14/src/tower.py
+++ pyweek14/src/tower.py
@@ -1,17 +1,21 @@
 import pygame, math
-import vista, foe, gamestate
+import vista, foe, gamestate, mechanics, data
 
 
 class Tower(object):
     color = 50, 125, 200
+    anchor = "center"
     def __init__(self, tech, (x, y)):
         self.element, self.invention = tech.split()
-        self.x, self.y = x, y
+        dx, dy = mechanics.footoffs[self.invention]
+        self.x, self.y = x + dx, y + dy
         self.chargetime = 3
         self.chargetimer = 0
         self.damage = 10
         self.range = 3
         self.t = 0
+        self.cfilter = mechanics.ecolors[self.element]
+        self.imgname = "tower"
 
     def attack(self, who):
         if self.element == "laser":
@@ -36,13 +40,18 @@ class Tower(object):
             if target: self.attack(target)
 
     def draw(self):
-        px, py = vista.mappos((self.x + 0.5, self.y + 0.5))
-        h = 25 * self.chargetimer / self.chargetime
-        for dtheta in (0, 2*math.pi/3, 4*math.pi/3):
-            theta = 10 * self.t + dtheta
-            dx, dy = 7 * math.sin(theta), 4 * math.cos(theta)
-            vista.mapwindow.set_at((int(px+dx), int(py+dy-h)), (255, 255, 255))
-        pygame.draw.line(vista.mapwindow, self.color, (px, py), (px, py-25), 5)
+        px, py = vista.mappos((self.x, self.y))
+        img = data.img(self.imgname, cfilter=self.cfilter)
+        data.draw(vista.mapwindow, img, (px, py), self.anchor)
+        
+
+
+#        h = 25 * self.chargetimer / self.chargetime
+#        for dtheta in (0, 2*math.pi/3, 4*math.pi/3):
+#            theta = 10 * self.t + dtheta
+#            dx, dy = 7 * math.sin(theta), 4 * math.cos(theta)
+#            vista.mapwindow.set_at((int(px+dx), int(py+dy-h)), (255, 255, 255))
+#        pygame.draw.line(vista.mapwindow, self.color, (px, py), (px, py-25), 5)
 
     
 
