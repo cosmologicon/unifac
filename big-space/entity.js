8a04dbfb852726d9ff1203d429fe709bc7248e07
Christopher Night <cosmologicon@gmail.com>
2012-05-19 17:52:03 -0400

    Big Space Effects module Saucer with speech bubbles

--- big-space/entity.js
+++ big-space/entity.js
@@ -8,6 +8,16 @@ var InSpace = {
     },
 }
 
+var Follows = {
+    init: function (parent) {
+        this.parent = parent
+    },
+    draw: function () {
+        context.translate(this.parent.x, this.parent.y)
+    },
+}
+
+
 var ApproachesTarget = {
     init: function (vmax) {
         this.targetx = this.x
@@ -20,10 +30,9 @@ var ApproachesTarget = {
         this.targetx = x
         this.targety = y
         if (this.targeteffect) {
-            effects.splice(effects.indexOf(this.targeteffect), 1)
+            this.targeteffect.die()
         }
         this.targeteffect = TargetEffect(x, y, this.color)
-        effects.push(this.targeteffect)
     },
     draw: function () {
         context.rotate(this.alpha)
@@ -37,7 +46,7 @@ var ApproachesTarget = {
         if (d <= d0) {
             this.x = this.targetx
             this.y = this.targety
-            effects.splice(effects.indexOf(this.targeteffect), 1)
+            this.targeteffect.die()
             this.targeteffect = null
         } else {
             this.x += dx * d0 / d
@@ -69,6 +78,34 @@ var DrawShip = {
     }
 }
 
+var DrawSaucer = {
+    init: function () {
+    },
+    draw: function () {
+        UFX.draw(context, "fs rgb(64,64,64) lw 1 ss rgb(128,128,128)")
+        UFX.draw(context, "[ z 2 1 b o 0 -6 4 ] f s")
+        UFX.draw(context, "( m 0 -6 l -12 -6 l -24 -4 q -40 4 0 4 q 40 4 24 -4 l 12 -6 ) f s")
+    }
+}
+
+var OscillatesWhenIdle = {
+    init: function (psi) {
+        this.beta = 0
+        this.psi = psi || 1
+        this.y0 = this.y
+    },
+    think: function (dt) {
+        if (!this.interacting) {
+            this.beta += this.psi * dt
+        }
+        this.y = this.y0 + 40 * Math.sin(this.beta)
+    },
+    draw: function () {
+        context.rotate(0.3 * Math.cos(this.beta))
+    },
+}
+
+
 var SendsDistress = {
     init: function () {
         this.distressed = false
@@ -82,7 +119,7 @@ var SendsDistress = {
                 this.distresst -= 0.4
                 var a = this.distressj * 2.39996
                 this.distressj += 1
-                effects.push(DistressCall(this.x, this.y, a))
+                DistressCall(this.x, this.y, a)
             }
         }
     },
@@ -130,55 +167,62 @@ var TimesOut = {
     think: function (dt) {
         this.dietimer -= dt
         if (this.dietimer <= 0) {
-            effects.splice(effects.indexOf(this), 1)
+            this.die()
         }
     }
 }
 
-var TextEffect = {
-    init: function (text, color, font) {
-        this.text = text
-        this.color = color || "white"
-        this.font = font || "30px 'Russo One'"
-    },
-    draw: function () {
-        context.scale(0.5, 1)
-        context.fillStyle = this.color
-        context.font = this.font
-        context.textAlign = "center"
-        context.fillText(this.text, 0, 0)
-    },
-}
-
-var IsSquare = {
-    init: function (r, color) {
-        this.r = r
-        this.color = color || "white"
-    },
-    draw: function () {
-        context.strokeStyle = this.color
-        context.lineWidth = 2
-        context.strokeRect(-this.r, -this.r, 2*this.r, 2*this.r)
-    }
-}
-
-var GrowingSquare = {
+var ShowsPlanetInfo = {
     init: function () {
-        this.color = "blue"
-        this.t = 0
+        this.infoeffect = undefined
+        this.interacting = false
+    },
+    interact: function (ship) {
+        this.interacting = true
     },
     think: function (dt) {
-        this.t += dt
-        if (this.t > 0.5) effects.splice(effects.indexOf(this), 1)
+        if (this.interacting) {
+            if (!this.infoeffect || effects.indexOf(this.infoeffect) < 0) {
+                this.infoeffect = PlanetInfoBox(this)
+            }
+        } else {
+            if (this.infoeffect) {
+                this.infoeffect.disappear()
+                if (effects.indexOf(this.infoeffect) < 0) {
+                    this.infoeffect = undefined
+                }
+            }
+        }
+        this.interacting = false
     },
-    draw: function () {
-        var p = this.t * 25
-        context.globalAlpha = Math.max(1. - (2 * this.t), 0)
-        context.strokeStyle = this.color
-        context.lineWidth = 2
-        context.strokeRect(-p, -p, 2*p, 2*p)
-    }
 }
+
+var ShowsSpeechBubble = {
+    init: function (info) {
+        this.bubble = undefined
+        this.interacting = false
+        this.info = info
+    },
+    interact: function (ship) {
+        this.interacting = true
+    },
+    think: function (dt) {
+        if (this.interacting) {
+            if (!this.bubble || effects.indexOf(this.bubble) < 0) {
+                this.bubble = SpeechBubble(this)
+            }
+        } else {
+            if (this.bubble) {
+                this.bubble.disappear()
+                if (effects.indexOf(this.bubble) < 0) {
+                    this.bubble = undefined
+                }
+            }
+        }
+        this.interacting = false
+    },
+}
+
 
 
 function Planet(x, y, r, color) {
@@ -186,6 +230,7 @@ function Planet(x, y, r, color) {
               .addcomp(InSpace, x, y)
               .addcomp(IsRound, r, color)
               .addcomp(SendsDistress)
+              .addcomp(ShowsPlanetInfo)
 }
 
 function Ship(x, y) {
@@ -196,29 +241,13 @@ function Ship(x, y) {
               .addcomp(DrawShip)
 }
 
-
-function DistressCall(x, y, a) {
+function Saucer(x, y, tip) {
     return UFX.Thing()
+              .definemethod("think")
               .addcomp(InSpace, x, y)
-              .addcomp(AtAngle, a)
-              .addcomp(Ascends, 600)
-              .addcomp(TimesOut, 20)
-              .addcomp(TextEffect, "HELP!", "blue")
-              
-}
-
-
-function ClickBox(x, y) {
-    return UFX.Thing()
-              .addcomp(InSpace, x, y)
-              .addcomp(GrowingSquare)
-}
-
-function TargetEffect(x, y, color) {
-    return UFX.Thing()
-              .addcomp(InSpace, x, y)
-              .addcomp(Spins, 3.0)
-              .addcomp(IsSquare, 4, color)
+              .addcomp(OscillatesWhenIdle, 2.5)
+              .addcomp(DrawSaucer)
+              .addcomp(ShowsSpeechBubble, ["blah", "blah blah blah"])
 }
 
 
