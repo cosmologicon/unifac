fbceff59d5949b569edd29a19a45429204699b3e
Christopher Night <cosmologicon@gmail.com>
2012-05-26 14:34:50 -0400

    Renamed to Twondy and Zoop throughout the cutscene text

--- twondy/cutscene.js
+++ twondy/cutscene.js
@@ -4,67 +4,67 @@ tip = []
 
 
 story[0] = [
-    "Once upon a time in deep space there was a world named Tondie. Tondie was a friendly world, but very small. It was so small, in fact, that only one creature lived on Tondie. Her name was Zupe.",
-    "Tondie and Zupe were the very best of friends, and not only because they didn't know anyone else. The fact is that they needed each other.",
-    "Zupe needed a place to stand, after all. And Tondie needed Zupe to protect it from the gray creatures from space that would come to hurt it."
+    "Once upon a time in deep space there was a world named Twondy. Twondy was a friendly world, but very small. It was so small, in fact, that only one creature lived on Twondy. Her name was Zoop.",
+    "Twondy and Zoop were the very best of friends, and not only because they didn't know anyone else. The fact is that they needed each other.",
+    "Zoop needed a place to stand, after all. And Twondy needed Zoop to protect it from the gray creatures from space that would come to hurt it."
 ]
 tip[0] = [
-    "Control Zupe with the arrow keys or WASD. You can double-jump in the air, and kick by pressing down, space, or enter.",
+    "Control Zoop with the arrow keys or WASD. You can double-jump in the air, and kick by pressing down, space, or enter.",
     "Kicking takes time to recharge. You can see the meter in the upper-right. You can destroy the invaders by kicking them, or jumping on them.",
-    "Zupe can't be hurt, but Tondie can!",
+    "Zoop can't be hurt, but Twondy can!",
 ]
 
 story[1] = [
-    "Zupe wanted to protect Tondie, of course, but it was also great fun! She soon discovered clever ways to help her ward off the invaders."
+    "Zoop wanted to protect Twondy, of course, but it was also great fun! She soon discovered clever ways to help her ward off the invaders."
 ]
 tip[1] = [
     "Structure unlocked: Tower. Place a tower to reach new heights!"
 ]
 
 story[2] = [
-    "Zupe discovered a kind of structure that would heal Tondie's wounds. It was very important to her that she keep her best friend feeling happy."
+    "Zoop discovered a kind of structure that would heal Twondy's wounds. It was very important to her that she keep her best friend feeling happy."
 ]
 tip[2] = [
-    "Structure unlocked: Healer. Recovers Tondie's health over time.",
+    "Structure unlocked: Healer. Recovers Twondy's health over time.",
     "Tip: If you're eager for the next wave, you can enter fast-forward mode by holding down Backspace."
 ]
 
 story[3] = [
-    "One day, Zupe made the most amazing discovery of all. By collecting enough, she could actually make Tondie grow in size! There would be so much room for activities!",
-    "Tondie was a little nervous about the idea at first, but finally agreed that it would be a good idea. So Zupe worked hard to collect enough."
+    "One day, Zoop made the most amazing discovery of all. By collecting enough, she could actually make Twondy grow in size! There would be so much room for activities!",
+    "Twondy was a little nervous about the idea at first, but finally agreed that it would be a good idea. So Zoop worked hard to collect enough."
 ]
 tip[3] = [
-    "To grow Tondie, keep the health above 90/100 and collect $400.",
+    "To grow Twondy, keep the health above 90/100 and collect $400.",
     "Tip: If you want to get a wide view, hold down Shift."
 ]
 
 story[4] = [
-    "Tondie was amazed at how it felt to be larger. It never felt so strong! Tondie asked Zupe to please collect even more so that Tondie could grow again.",
+    "Twondy was amazed at how it felt to be larger. It never felt so strong! Twondy asked Zoop to please collect even more so that Twondy could grow again.",
     "They both agreed that this was the most fun they'd ever had together."
 ]
 tip[4] = [
-    "You can now purchase upgrades for your abilities and the structures you've built. You can grow Tondie again when you collect $600."
+    "You can now purchase upgrades for your abilities and the structures you've built. You can grow Twondy again when you collect $600."
 ]
 
 
 
 story[5] = [
-    "Tondie was getting very big indeed, and the invaders seemed to be getting more and more fierce. Fortunately, Zupe's ingenuity persisted."
+    "Twondy was getting very big indeed, and the invaders seemed to be getting more and more fierce. Fortunately, Zoop's ingenuity persisted."
 ]
 tip[5] = [
     "Structure unlocked: Bubbler. Creates bubbles to give you a boost when you need it."
 ]
 
 story[6] = [
-    "Tondie was finally starting to feel like a real planet. This had never happened before. It was a strange feeling, but Tondie knew this is what it wanted."
+    "Twondy was finally starting to feel like a real planet. This had never happened before. It was a strange feeling, but Twondy knew this is what it wanted."
 ]
 tip[6] = [
     "Structure unlocked: Launcher. Bounce off this to get a mighty boost."
 ]
 
 story[7] = [
-    "Zupe asked Tondie if it thought it was big enough now. When Tondie answered, Zupe almost didn't recognize it.",  
-    "Instead of the voice of her friend, Zupe heard only a deep growl... MOOOOOREEEE PLEEEASE.",
+    "Zoop asked Twondy if it thought it was big enough now. When Twondy answered, Zoop almost didn't recognize it.",  
+    "Instead of the voice of her friend, Zoop heard only a deep growl... MOOOOOREEEE PLEEEASE.",
     "She was surprised, but continued on her quest for more.",
 ]
 tip[7] = [
@@ -72,24 +72,24 @@ tip[7] = [
 ]
 
 story[8] = [
-    "Tondie now grew so large that the Lord of All Invaders became jealous. The Invader's capital ship was dispatched to cut Tondie back down to size.",
-    "Zupe knew that this was her ultimate test. If they defeated the capital ship, they would never be bothered again.",
-    "Tondie said nothing."
+    "Twondy now grew so large that the Lord of All Invaders became jealous. The Invader's capital ship was dispatched to cut Twondy back down to size.",
+    "Zoop knew that this was her ultimate test. If they defeated the capital ship, they would never be bothered again.",
+    "Twondy said nothing."
 ]
 tip[8] = [
     "Good luck!"
 ]
 
 story[9] = [
-    "We did it, Tondie! yelled Zupe. We'll be safe forever now!",
-    "But Tondie didn't say anything.",
-    "Tondie was no longer the tiny world that Zupe knew. Tondie was a full planet now. And planets live differently than you and I.",
+    "We did it, Twondy! yelled Zoop. We'll be safe forever now!",
+    "But Twondy didn't say anything.",
+    "Twondy was no longer the tiny world that Zoop knew. Twondy was a full planet now. And planets live differently than you and I.",
     "Planets don't feel the passage of time, or the course of events on their surface.",
     "Planets don't have friends.",
     "...",
     "News spread throughout space of the planet where the Lord of All Invaders was defeated.",
-    "Before long, Zupe was not the only one living on Tondie. She saw many weary people come to this world of peace and happiness.",
-    "Tondie might never be her friend again, but it would forever be her home.",
+    "Before long, Zoop was not the only one living on Twondy. She saw many weary people come to this world of peace and happiness.",
+    "Twondy might never be her friend again, but it would forever be her home.",
 ]
 tip[9] = [
     "Thanks for playing!",
