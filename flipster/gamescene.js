4ab6b5585e61688def27841f7c4d7b46427a793c
Christopher Night <cosmologicon@gmail.com>
2012-08-04 18:24:35 -0400

    Flipster level module

--- flipster/gamescene.js
+++ flipster/gamescene.js
@@ -10,22 +10,6 @@ GameScene.start = function () {
         oldx: 200,
         oldy: 100,
     }
-    this.blocks = [[100, 300, 100, 100], [400, 400, 250, 50]]
-    this.points = []
-    
-    for (var j = 0 ; j < this.blocks.length ; ++j) {
-        var block = this.blocks[j]
-        var x = block[0], y = block[1], w = block[2], h = block[3]
-        var nw = Math.floor(w / 8) + 1, nh = Math.floor(h / 8) + 1
-        for (var k = 0 ; k <= nw ; ++k) {
-            this.points.push([x + k * w / nw, y])
-            this.points.push([x + k * w / nw, y+h])
-        }
-        for (var k = 1 ; k < nh ; ++k) {
-            this.points.push([x, y + k * h / nh])
-            this.points.push([x + w, y + k * h / nh])
-        }
-    }
     this.tstill = 0
     
     this.csize = 1
@@ -34,16 +18,16 @@ GameScene.start = function () {
     this.buffer.width = settings.sx
     this.buffer.height = settings.sy
     
+    
+    this.level = level1
+    this.points = this.level.points()
     this.drawbuffer()
-    
-    
 }
 
 GameScene.drawbuffer = function () {
-    UFX.draw(this.buffercon, "fs blue fr", 0, 0, settings.sx, settings.sy, "fs rgb(0,1,100)")
-    for (var j = 0 ; j < this.blocks.length ; ++j) {
-        UFX.draw(this.buffercon, "fr", this.blocks[j])
-    }
+    UFX.draw(this.buffercon, "fs rgb(0,0,20) fr", 0, 0, settings.sx, settings.sy)
+    this.level.trace(this.buffercon)
+    UFX.draw(this.buffercon, "fs rgb(0,1,100) f")
 }
 
 GameScene.thinkargs = function (dt) {
@@ -64,25 +48,38 @@ GameScene.think = function (dt, mpos, clicked) {
         this.ball.vx = 0
         this.ball.vy = 0
     }
-    this.ball.vy += mechanics.g * dt
-    this.ball.x += this.ball.vx * dt
-    this.ball.y += this.ball.vy * dt
     
     var ball = this.ball
-    this.points.forEach(function (point) {
-        var x0 = point[0], y0 = point[1]
-        var dx = x0 - ball.x, dy = y0 - ball.y
-        if (dx * dx + dy * dy > ball.R * ball.R) return
-        if (dx * ball.vx + dy * ball.vy <= 0) return
-        var p = -(1 + mechanics.elasticity) * (ball.vx * dx + ball.vy * dy) / (dx * dx + dy * dy)
-        ball.vx += p * dx
-        ball.vy += p * dy
-        var df = ball.R / Math.sqrt(dx * dx + dy * dy) - 1
-        if (df > 0) {
-            ball.x -= df * dx
-            ball.y -= df * dy
-        }
+    var x = ball.x, y = ball.y
+    var v = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy)
+    var Rplus = 1 + ball.R + 1.2 * dt * v
+    var Rplus2 = Rplus * Rplus
+    
+    var closepoints = this.points.filter(function(p) {
+        return (p[0] - x) * (p[0] - x) + (p[1] - y) * (p[1] - y) < Rplus2
     })
+
+    for (var jt = 0 ; jt < 10 ; ++jt) {
+        ball.vy += mechanics.g * dt / 10.
+        ball.x += ball.vx * dt / 10.
+        ball.y += ball.vy * dt / 10.
+        
+        closepoints.forEach(function (point) {
+            var x0 = point[0], y0 = point[1]
+            var dx = x0 - ball.x, dy = y0 - ball.y
+            if (dx * dx + dy * dy > ball.R * ball.R) return
+            if (dx * ball.vx + dy * ball.vy <= 0) return
+            var p = -(1 + mechanics.elasticity) * (ball.vx * dx + ball.vy * dy) / (dx * dx + dy * dy)
+            ball.vx += p * dx
+            ball.vy += p * dy
+            var df = ball.R / Math.sqrt(dx * dx + dy * dy) - 1
+            if (df > 0) {
+                ball.x -= df * dx
+                ball.y -= df * dy
+            }
+        })
+    }
+
     
     var dx = this.ball.x - oldx, dy = this.ball.y - oldy
     if (dx * dx + dy * dy < 0.01) {
@@ -115,12 +112,14 @@ GameScene.think = function (dt, mpos, clicked) {
         var n = Math.floor(this.csize / 2) + 10
         for (var j = 0 ; j < n ; ++j) {
             var theta = 2 * Math.PI * j / n
-            var x = Math.floor(mpos[0] + this.csize * Math.cos(theta) + 0.5)
+            var x0 = Math.floor(mpos[0] + this.csize * Math.cos(theta) + 0.5)
+            var x1 = Math.floor(mpos[0] - this.csize * Math.cos(theta) + 0.5)
             var y = Math.floor(mpos[1] + this.csize * Math.sin(theta) + 0.5)
-            var g = data[(settings.sx * y + x) * 4 + 1]
-            if (g) {
-                this.points.push([x, y])
-                this.points.push([2 * mpos[0] - x, y])
+            var g0 = 0 <= x0 && x0 < settings.sx && data[(settings.sx * y + x0) * 4 + 1]
+            var g1 = 0 <= x1 && x1 < settings.sx && data[(settings.sx * y + x1) * 4 + 1]
+            if (g0 && !g1) {
+                this.points.push([x0, y])
+                this.points.push([x1, y])
             }
         }
         this.points = this.points.filter(function (point) { return point[0] >= 0 && point[0] < settings.sx })
@@ -131,12 +130,13 @@ GameScene.think = function (dt, mpos, clicked) {
     if (mpos) {
         UFX.draw("b o", mpos, this.csize, "ss white lw 1 s")
     }
-    UFX.draw("b")
-    this.points.forEach(function (point) {
-        UFX.draw("o", point[0], point[1], 2)
-    })
-    UFX.draw("fs red f")
-    
+    if (settings.showpoints) {
+        UFX.draw("b")
+        this.points.forEach(function (point) {
+            UFX.draw("o", point[0], point[1], 2)
+        })
+        UFX.draw("fs red f")
+    }    
 
     UFX.draw("b o", this.ball.x, this.ball.y, this.ball.R, "fs red f ss black lw 1.5 s")
     
