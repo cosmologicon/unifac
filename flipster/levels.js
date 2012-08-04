30f2e9e1be799508b8bfe156b438103ad5a14a79
Christopher Night <cosmologicon@gmail.com>
2012-08-04 18:24:35 -0400

    Flipster level module

---
+++ flipster/levels.js
@@ -0,0 +1,40 @@
+
+function Level() {
+    this.blocks = []
+    for (var j = 0 ; j < arguments.length ; ++j) {
+        this.blocks.push(arguments[j])
+    }
+}
+Level.prototype = {
+    points: function () {
+        var ps = []
+        for (var j = 0 ; j < this.blocks.length ; ++j) {
+            var block = this.blocks[j]
+            for (var k = 0 ; k < block.length ; ++k) {
+                var k2 = (k + 1) % block.length
+                var x0 = block[k][0], y0 = block[k][1]
+                var x1 = block[k2][0], y1 = block[k2][1]
+                var dx = x1 - x0, dy = y1 - y0
+                var d = Math.sqrt(dx * dx + dy * dy)
+                var n = Math.floor(d / 8) + 1
+                for (var h = 0 ; h < n ; ++h) {
+                    ps.push([x0 + h * dx / n, y0 + h * dy / n])
+                }
+            }
+        }
+        return ps
+    },
+    trace: function (context) {
+        UFX.draw(context, "b")
+        for (var j = 0 ; j < this.blocks.length ; ++j) {
+            var block = this.blocks[j]
+            UFX.draw(context, "m", block[block.length - 1])
+            for (var k = 0 ; k < block.length ; ++k) {
+                UFX.draw(context, "l", block[k])
+            }
+        }
+    },
+}
+
+var level1 = new Level([[100, 400], [500, 300], [220, 600]])
+
--- flipster/settings.js
+++ flipster/settings.js
@@ -1,12 +1,15 @@
 var settings = {
     sx: 700,
     sy: 600,
+    showpoints: true,  // show collision points (for debugging)
 }
 
 var mechanics = {
     ballR: 25,
     g: 300,
     elasticity: 0.4,
-    apgrate: 20,  // aperture growth rate
-    amax: 120,     // max aperture size
+    apgrate: 40,  // aperture growth rate
+    amax: 160,     // max aperture size
 }
+
+
