de4393ecbf592ebb066d64f3f82889300535cd4f
Christopher Night <cosmologicon@gmail.com>
2012-08-04 16:21:50 -0400

    Flipster: flip points on click

--- flipster/gamescene.js
+++ flipster/gamescene.js
@@ -6,9 +6,12 @@ GameScene.start = function () {
         y: 100,
         vx: 0,
         vy: 0,
-        R: 40,
+        R: mechanics.ballR,
+        oldx: 200,
+        oldy: 100,
     }
-    this.points = UFX.random.spread(40, 0.15, 600, 400, 0, 200)
+    this.points = UFX.random.spread(500, 0.15, settings.sx, 400, 0, 200)
+    this.tstill = 0
 }
 
 GameScene.thinkargs = function (dt) {
@@ -18,26 +21,24 @@ GameScene.thinkargs = function (dt) {
     })
     return [dt, UFX.mouse.pos, clicked]
 }
-GameScene.think = function (dt, mpos, clicked) {
-    UFX.draw("fs blue fr 0 0", settings.sx, settings.sy)
-
-    if (mpos) {
-        UFX.draw("b o", mpos, 10, "ss white lw 1 s")
+GameScene.think = function (dt, mpos, clicked) {    
+    
+    if (mpos && clicked) {
+        this.points.forEach(function (point) {
+            var dx = point[0] - mpos[0], dy = point[1] - mpos[1]
+            if (dx * dx + dy * dy > 100 * 100) return
+            point[0] -= 2 * dx
+        })
     }
     
-    UFX.draw("b")
-    this.points.forEach(function (point) {
-        UFX.draw("o", point[0], point[1], 2)
-    })
-    UFX.draw("fs white f")
-    
-    if (clicked) {
-        this.ball.x = mpos[0]
-        this.ball.y = mpos[1]
+    var oldx = this.ball.x, oldy = this.ball.y
+    if (this.tstill > 0.5 || this.ball.y > settings.sy + 100) {
+        this.ball.x = UFX.random(100, settings.sx - 100)
+        this.ball.y = 40
         this.ball.vx = 0
         this.ball.vy = 0
     }
-    this.ball.vy += 100 * dt
+    this.ball.vy += mechanics.g * dt
     this.ball.x += this.ball.vx * dt
     this.ball.y += this.ball.vy * dt
     
@@ -47,7 +48,7 @@ GameScene.think = function (dt, mpos, clicked) {
         var dx = x0 - ball.x, dy = y0 - ball.y
         if (dx * dx + dy * dy > ball.R * ball.R) return
         if (dx * ball.vx + dy * ball.vy <= 0) return
-        var p = -1.5 * (ball.vx * dx + ball.vy * dy) / (dx * dx + dy * dy)
+        var p = -(1 + mechanics.elasticity) * (ball.vx * dx + ball.vy * dy) / (dx * dx + dy * dy)
         ball.vx += p * dx
         ball.vy += p * dy
         var df = ball.R / Math.sqrt(dx * dx + dy * dy) - 1
@@ -57,6 +58,26 @@ GameScene.think = function (dt, mpos, clicked) {
         }
     })
     
+    var dx = this.ball.x - oldx, dy = this.ball.y - oldy
+    if (dx * dx + dy * dy < 0.01) {
+        this.tstill += dt
+    } else {
+        this.tstill = 0
+    }
+
+
+    UFX.draw("fs blue fr 0 0", settings.sx, settings.sy)
+    if (mpos) {
+        UFX.draw("b o", mpos, 100, "ss white lw 1 s")
+    }
+
+    UFX.draw("b")
+    this.points.forEach(function (point) {
+        UFX.draw("o", point[0], point[1], 2)
+    })
+    UFX.draw("fs white f")
+
+
     UFX.draw("b o", this.ball.x, this.ball.y, this.ball.R, "fs red f ss black lw 1.5 s")
     
 
--- flipster/settings.js
+++ flipster/settings.js
@@ -2,3 +2,9 @@ var settings = {
     sx: 700,
     sy: 600,
 }
+
+var mechanics = {
+    ballR: 25,
+    g: 300,
+    elasticity: 0.4,
+}
