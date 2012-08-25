cd885cfd0a20f5b901f7857d9431ffdfc7615234
Christopher Night <cosmologicon@gmail.com>
2012-08-25 01:56:48 -0400

    Mobbels - island objects with stroke/fill effects

--- mobbel/gamescene.js
+++ mobbel/gamescene.js
@@ -13,14 +13,17 @@ GameScene.think = function (dt, kpressed) {
 
 GameScene.draw = function () {
     UFX.draw("fs black fr 0 0", settings.sx, settings.sy)
-    UFX.draw("( m -1000 0")
+/*    UFX.draw("( m -1000 0")
     for (var j = 0 ; j < settings.sx ; j += 1) {
         var x = camera.xmin + j
         var h = getheight(x)
         var y = settings.sy - camera.y0 - 200 * h
         UFX.draw("l", j, y)
     }
-    UFX.draw("l", settings.sx, "1000 l 0 1000 ) fs orange f")
-    UFX.draw("[ alpha 0.5 fs purple fr 0", settings.sy - camera.y0 + 20, settings.sx, settings.sy, "]")
+    UFX.draw("l", settings.sx, "1000 l 0 1000 ) fs orange f")*/
+    UFX.draw("[ t", -camera.x0, settings.sy - camera.y0, "vflip")
+    islands.forEach(function (island) { island.draw() })
+//    UFX.draw("[ alpha 0.5 fs purple fr 0", settings.sy - camera.y0 + 20, settings.sx, settings.sy, "]")
+    UFX.draw("]")
 }
 
