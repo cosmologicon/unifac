f2052a4bbec474b422618ed9881286f10b0a680b
Christopher Night <cosmologicon@gmail.com>
2012-08-25 01:56:48 -0400

    Mobbels - island objects with stroke/fill effects

--- mobbel/settings.js
+++ mobbel/settings.js
@@ -1,6 +1,7 @@
 var settings = {
     sx: 800,
     sy: 300,
+    hfac: 0.15,  // horizon factor for islands
 }
 
 var mechanics = {
--- mobbel/terrain.js
+++ mobbel/terrain.js
@@ -1,7 +1,8 @@
 
 
-var nheight = 16384, palt = 10
-var altx0 = nheight * palt
+var nheight = 16384, twidth = 10
+var sealevel = -0.2
+var altx0 = nheight * twidth
 var heights = UFX.noise.wrap2d([nheight, 1], [256,8])
 
 
@@ -19,20 +20,70 @@ function fractalize(h) {
     return h
 }
 heights = fractalize(heights)
+for (var j = 0 ; j < heights.length ; ++j) {
+    heights[j] -= sealevel
+    heights[j] *= 200
+}
 
 
 function getheight(x) {
     x = (x % altx0 + altx0) % altx0
-    var n0 = Math.floor(x / palt) % nheight
+    var n0 = Math.floor(x / twidth) % nheight
     var n1 = (n0 + 1) % nheight
-    var d = x / palt - n0
+    var d = x / twidth - n0
     return heights[n0] * (1-d) + heights[n1] * d
 }
 
 
-function Island() {
+// the island that's above sea level at coordinate x
+function Island(x) {
+    var n0 = Math.floor(x / twidth + 0.5)
+    while (getheight((n0 - 1) * twidth) > 0) n0 -= 1
+    var n1 = n0
+    while (getheight((n1 + 1) * twidth) > 0) n1 += 1
+    this.n0 = n0  // min index that's above sea level
+    this.n1 = n1  // max index that's above sea level
+    var d = getheight(n0*twidth) / (getheight(n0*twidth)-getheight((n0-1)*twidth))
+    this.xmin = ((n0-1)*d + n0*(1-d)) * twidth
+    d = getheight(n1*twidth) / (getheight(n1*twidth)-getheight((n1+1)*twidth))
+    this.xmax = ((n1+1)*d + n1*(1-d)) * twidth
+    this.x = [this.xmin]
+    this.y = [0]
+    for (var n = n0 ; n <= n1 ; ++n) {
+        var x = n * twidth, y = getheight(x)
+        this.x.push(x)
+        this.y.push(y)
+    }
+    this.x.push(this.xmax)
+    this.y.push(0)
+    this.n = this.x.length
+}
+Island.prototype = {
+    isvisible: function () {
+        return this.xmax > camera.xmin || this.xmin < camera.xmax
+    },
+    trace: function (pfac, nfac) {
+        UFX.draw("( m", this.x[0], this.y[0])
+        for (var j = 1 ; j < this.n ; ++j) {
+            UFX.draw("l", this.x[j], this.y[j] * pfac)
+        }
+        for (var j = this.n - 1 ; j >= 1 ; --j) {
+            UFX.draw("l", this.x[j], -this.y[j] * nfac)
+        }
+        UFX.draw(")")
+    },
+    draw: function () {
+        UFX.draw("[")
+        this.trace(1, settings.hfac)
+        UFX.draw("clip fs rgb(120,60,0) f ] ss rgb(180,90,0) lw 6 s")
+    },
+}
+
+function explore(x0, x1) {
 
 }
+var islands = [new Island(0)]
+
 
 
 
