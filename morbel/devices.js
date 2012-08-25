f0b28b8525e89cae2637332fdc8576bfcca42f6a
Christopher Night <cosmologicon@gmail.com>
2012-08-25 14:31:51 -0400

    Morbels Basic device that can be charged with 3 Hoppers

---
+++ morbel/devices.js
@@ -0,0 +1,28 @@
+
+
+
+function Device(x) {
+    this.x = x
+    this.y = getheight(x)
+    this.charges = 0
+    this.alive = true
+    this.think(0)
+}
+Device.prototype = UFX.Thing()
+    .definemethod("think")
+    .addcomp(Earthbound)
+    .addcomp(HorizontalClipping)
+    .addcomp(StandsUpward)
+    .addcomp({
+        charge: function () {
+            this.charges = Math.min(this.charges + 1, 3)
+        },
+        draw: function () {
+            UFX.draw("( m -20 60 l 20 60 l 30 -30 l -30 -30 ) fs rgb(64,64,64) ss gray lw 2 f s")
+            for (var j = 0 ; j < 3 ; ++j) {
+                UFX.draw("b o", (j-1)*15, "70 6 fs", (this.charges > j ? "green" : "red"), "f")
+            }
+        },
+    })
+
+
