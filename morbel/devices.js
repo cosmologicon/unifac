9958f4ac3b7c1ef575437b2eb2d1bdd8afbbceee
Christopher Night <cosmologicon@gmail.com>
2012-08-26 13:37:52 -0400

    Balancing up through stage 3

--- morbel/devices.js
+++ morbel/devices.js
@@ -1,12 +1,12 @@
 
 
-
 function Device(x) {
     this.x = x
     this.y = getheight(x)
     this.charges = 0
     this.alive = true
     this.think(0)
+    this.h = 30
 }
 Device.prototype = UFX.Thing()
     .definemethod("think")
@@ -38,6 +38,7 @@ function Tower(x) {
     this.charges = 0
     this.alive = true
     this.think(0)
+    this.h = 160
 }
 Tower.prototype = UFX.Thing()
     .definemethod("think")
@@ -46,16 +47,20 @@ Tower.prototype = UFX.Thing()
     .addcomp(DisappearsUnderwater)
     .addcomp(StandsUpward)
     .addcomp({
-        charge: function () {
-            this.charges = Math.min(this.charges + 1, 3)
+        charge: function (n) {
+            n = n || 1
+            if (this.charges < 3) playsound("poweron")
+            this.charges = Math.min(this.charges + n, 3)
         },
         draw: function () {
             UFX.draw("fs rgb(64,64,64) ss gray lw 2")
-            UFX.draw("( m -8 40 l -8 140 l 8 140 l 8 40 ) f s")
+            UFX.draw("( m -3 110 l -1 160 l 1 160 l 3 110 ) f s")
+            UFX.draw("( m -8 40 l -8 120 l 8 120 l 8 40 ) f s")
             UFX.draw("( m -10 80 l 10 80 l 14 -20 l -14 -20 ) f s")
-//            for (var j = 0 ; j < 3 ; ++j) {
-//                UFX.draw("b o", (j-1)*15, "70 6 fs", (this.charges > j ? "green" : "red"), "f")
-//            }
+            for (var j = 2 ; j >= 0 ; --j) {
+                var r = Math.sqrt(j+1) * 9
+                UFX.draw("b o 0 160", r, "lw 1 fs", (this.charges > j ? "green" : "rgb(100,0,0)"), "f s")
+            }
         },
     })
 
@@ -87,5 +92,38 @@ Glower.prototype = UFX.Thing()
         },
     })
     
+function Barrier(x) {
+    this.x = x
+    this.y = getheight(x)
+    this.alive = true
+    this.pleft = You.x < this.x
+    this.think(0)
+    this.h = 0
+}
+Barrier.prototype = UFX.Thing()
+    .definemethod("charge")
+    .addcomp(Earthbound)
+    .addcomp(HorizontalClipping)
+    .addcomp(DisappearsUnderwater)
+    .addcomp({
+        tilt: function () { return 0 },
+        think: function (dt) {
+            if (You.y < 100 + this.y) {
+                if (this.pleft) {
+                    You.x = Math.min(You.x, this.x - 12)
+                } else {
+                    You.x = Math.max(You.x, this.x + 12)
+                }
+            } else {
+                this.pleft = You.x < this.x
+            }
+        },
+        draw: function () {
+            UFX.draw("( m -4 -20 l -4 100 l 4 100 l 4 -20 ) fs rgb(24,24,24) ss gray lw 1 f s")
+        },
+    })
+
+
+
 
 
--- morbel/gamestate.js
+++ morbel/gamestate.js
@@ -1,42 +1,47 @@
 var gamestate = {
-    stage: 1,
+    stage: 3,
     getpeaks: function () {
         if (this.stage == 0) {
-            return [[0, 500, 260], [700, 200, 120]]
+            return [[300, 500, 100], [-300, 300, 150], [-800, 300, 100]]
         } else if (this.stage == 1) {
             return [[0, 400, 220], [600, 240, 140], [-600, 200, 160]]
         } else if (this.stage == 2) {
             return [[0, 300, 180], [1100, 400, 240], [-900, 350, 220]]
         } else if (this.stage == 3) {
-            return [[0, 600, 320]]
+            return [[0, 600, 240], [1400, 400, 150], [2200, 400, 350]]
         } else if (this.stage == 4) {
             return [[300, 400, 180], [1700, 200, 130], [2400, 500, 280]]
-        } else if (this.stage == 6) {
-            return [[0, 400, 300]]
+        } else if (this.stage == 5) {
+            return [[-300, 300, 140], [-300, 600, 100], [1100, 400, 200], [2000, 400, 200]]
         } else if (this.stage == 7) {
             return [[0, 300, 260]]
         }
     },
     getdevices: function () {
         if (this.stage == 0) {
-            return []
+            return [new Device(-150)]
         } else if (this.stage == 1) {
             return [new Device(-600), new Device(380)]
         } else if (this.stage == 2) {
             return [new Device(-1100), new Device(1100)]
         } else if (this.stage == 3) {
-            return [new Tower(0)]
+            return [new Tower(0), new Tower(2000)]
         } else if (this.stage == 4) {
             return [new Device(330), new Device(2300)]
-        } else if (this.stage == 6) {
-            return []
+        } else if (this.stage == 5) {
+            return [new Device(-550), new Device(1550), new Barrier(-300), new Barrier(1200), new Barrier(1900)]
         } else if (this.stage == 7) {
             return []
         }
     },
     generatemorbels: function (dt) {
         if (this.stage == 0) {
-            // no morbels in stage 0
+            if (UFX.random(2) < dt) {
+                var x = UFX.random(-1000, 1000)
+                if (Math.abs(x - 150) > 350) {
+                    morbels.push(new Zapper(x, 450))
+                }
+            }
         } else if (this.stage == 1) {
             morbels = morbels.filter(function (m) { return Math.abs(m.x) < 1200 })
             if (morbels.length < 8 && UFX.random(2) < dt) {
@@ -51,10 +56,9 @@ var gamestate = {
                 morbels.push(new Flopper(UFX.random.choice([-600, 700])))
             }
         } else if (this.stage == 3) {
-            morbels = morbels.filter(function (m) { return Math.abs(m.x) < 1800 })
-            if (morbels.length < 18 && UFX.random(2) < dt) {
-                morbels.push(new Flapper(UFX.random(-500, 500), UFX.random(200, 400)))
-            }
+            if (morbels.length < 20) dt = 5
+            if (UFX.random(5) < dt) morbels.push(new Flopper(800))
+            if (UFX.random(1) < dt) morbels.push(new Flapper(UFX.random(-1100, 2800), 600))
         } else if (this.stage == 4) {
             morbels = morbels.filter(function (m) { return m.x > -400 && m.x < 3200 })
             while (morbels.length < 24) {
@@ -62,10 +66,22 @@ var gamestate = {
                 if (getheight(x) < -20) {
                     morbels.push(new Whipper(x))
                 }
-                if (UFX.random() < 0.1) {
-                    morbels.push(new Flopper(1000))
-                }
             }
+        } else if (this.stage == 5) {
+            morbels = morbels.filter(function (m) { return m.x > -1100 && m.x < 2600 })
+            if (morbels.length < 12) dt = 5
+            if (UFX.random(5) < dt) morbels.push(new Gripper(2300))
+            if (UFX.random(5) < dt) morbels.push(new Gripper(-800))
+            if (UFX.random(2) < dt) morbels.push(new Gripper(200))
+            if (UFX.random(5) < dt) morbels.push(new Gripper(600))
+            if (UFX.random(10) < dt) morbels.push(new Whipper(600))
+            if (UFX.random(10) < dt) morbels.push(new Whipper(2300))
+            if (UFX.random(5) < dt) morbels.push(new Hopper(-800))
+            if (UFX.random(5) < dt) morbels.push(new Hopper(200))
+            if (UFX.random(5) < dt) morbels.push(new Hopper(600))
+            if (UFX.random(5) < dt) morbels.push(new Hopper(2300))
+            if (UFX.random(5) < dt) morbels.push(new Flopper(400))
+            if (UFX.random(4) < dt) morbels.push(new Flapper(UFX.random(-1100, 2600), 600))
         } else if (this.stage == 6) {
             if (!morbels.length) {
                 morbels.push(new Yapper(-200, "So you're God, huh? I figured you'd be taller."))
@@ -90,9 +106,12 @@ var gamestate = {
     },
     checkwin: function () {
         if (this.stage == 0) {
+            return devices[0].charges >= 3
         } else if (this.stage == 1) {
             return devices[0].charges >= 3 && devices[1].charges >= 3
         } else if (this.stage == 2) {
+            return devices[0].charges >= 3 && devices[1].charges >= 3
+        } else if (this.stage == 3) {
             return devices[0].charges >= 3 && devices[1].charges >= 3
         } else if (this.stage == 4) {
             return devices[0].charges >= 3 && devices[1].charges >= 3
