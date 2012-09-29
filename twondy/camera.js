7ea02a5353a068d9543411c1c7820a82d3b1ce40
Christopher Night <cosmologicon@gmail.com>
2012-09-29 15:23:49 -0400

    Twondy and Zoop: x -> X implement first draft of camera.isvisible

--- twondy/camera.js
+++ twondy/camera.js
@@ -1,8 +1,8 @@
 
 var camera = {
-    x: 0,
+    X: 0,
     y: 0,
-    targetx: 0,
+    targetX: 0,
     targety: 0,
     zoom: 1,
     ymin: 40,
@@ -10,11 +10,11 @@ var camera = {
     chasezoom: false,
     
     lookat: function (p) {
-        this.x = this.targetx = p[0]
+        this.X = this.targetX = p[0]
         this.y = this.targety = p[1]
     },
     settarget: function (p) {
-        this.targetx = p[0]
+        this.targetX = p[0]
         if (this.mode === "play") {
             this.targety = Math.max(p[1], this.ymin)
         } else if (this.mode === "planet") {
@@ -24,23 +24,23 @@ var camera = {
     },
     think: function (dt) {
         var f = 1 - Math.exp(-2.5 * dt)
-        var dx = this.targetx - this.x, dy = this.targety - this.y
-        dx = ((dx + tau/2) % tau + tau) % tau - tau/2
+        var dX = getdX(this.X, this.targetX), dy = this.targety - this.y
         if (this.targety > 1 + this.ymin) {
             var r = gamestate.worldr
-            var px = (this.y + r) * Math.sin(this.x), py = (this.y + r) * Math.cos(this.x)
-            var tx = (this.targety + r) * Math.sin(this.targetx), ty = (this.targety + r) * Math.cos(this.targetx)
+            var px = (this.y + r) * Math.sin(this.X), py = (this.y + r) * Math.cos(this.X)
+            var tx = (this.targety + r) * Math.sin(this.targetX), ty = (this.targety + r) * Math.cos(this.targetX)
             px += f * (tx - px)
             py += f * (ty - py)
-            this.x = Math.atan2(px, py)
+            this.X = Math.atan2(px, py)
             this.y = Math.sqrt(px * px + py * py) - r
-        } else if (Math.abs(dx) < 0.01 && Math.abs(dy) < 1) {
-            this.x = this.targetx
+        } else if (Math.abs(dX) < 0.01 && Math.abs(dy) < 1) {
+            this.X = this.targetX
             this.y = this.targety
         } else {
-            this.x += f * dx
+            this.X += f * dX
             this.y += f * dy
         }
+        this.p0 = getpos(this.X, this.y)
         var z
         if (this.mode === "play") {
             z = Math.min(Math.max(0.4 * settings.sy / Math.max(1, this.y + gamestate.worldr), 0.1), 10)
@@ -66,12 +66,15 @@ var camera = {
         context.translate(settings.sx/2, settings.sy/2)
         context.scale(this.zoom, -this.zoom)
         context.translate(0, -this.y - gamestate.worldr)
-        context.rotate(this.x)
+        context.rotate(this.X)
     },
     // Is the position (X, y) within d units of a point that's currently visible?
-    isonscreen: function (X, y, d) {
-        r = r || 0
-        return true
+    // TODO: needs work
+    isvisible: function (X, y, d) {
+        var p = getpos(X, y)
+        var dx = p[0] - this.p0[0], dy = p[1] - this.p0[1]
+        var r = (300 + d) / this.zoom
+        return dx * dx + dy * dy < r * r
     },
 }
 
