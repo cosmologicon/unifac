91840ca9ae19ce612fa02b4fb05dbee32a3f6a76
Christopher Night <cosmologicon@gmail.com>
2012-11-12 08:23:21 -0500

    Mortimer the Lepidopterist Filled in HUD along the bottom

--- mort/effect.js
+++ mort/effect.js
@@ -43,8 +43,11 @@ var FillStroke = {
 }
 
 var AnchorBottomLeft = {
+	init: function (dx, dy) {
+		this.dx = dx || 0 ; this.dy = dy || 0
+	},
 	draw: function () {
-		UFX.draw("textalign left textbaseline bottom")
+		UFX.draw("textalign left textbaseline bottom t", this.dx, this.dy)
 	},
 }
 var AnchorBottomRight = {
@@ -55,27 +58,119 @@ var AnchorBottomRight = {
 		UFX.draw("textalign right textbaseline bottom t", this.dx, this.dy)
 	},
 }
-
-
-var ActionHUD = {
-	bankindicator: UFX.Thing()
-		.addcomp(AnchorBottomRight, settings.sx - 5, settings.sy - 5)
-		.addcomp(FillStroke, "bold 40px sans-serif", "white", "black", 2)
-		.addcomp(NoCache)
-		.addcomp({
-			think: function (dt) { this.settext("\u00A3" + gamestate.catchamount + "/" + gamestate.goal) }
-		})
-	,
-	levelinit: function () {
-	},
-	think: function (dt) {
-		this.bankindicator.think(dt)
+var AnchorCenter = {
+	init: function (dx, dy) {
+		this.dx = dx || 0 ; this.dy = dy || 0
 	},
 	draw: function () {
-		function draw(e) { context.save() ; e.draw() ; context.restore() }
-		draw(this.bankindicator)
+		UFX.draw("textalign center textbaseline middle t", this.dx, this.dy)
+	},
+}
+var AnchorBottomCenter = {
+	init: function (dx, dy) {
+		this.dx = dx || 0 ; this.dy = dy || 0
+	},
+	draw: function () {
+		UFX.draw("textalign center textbaseline bottom t", this.dx, this.dy)
+	},
+}
+
+var Float = {
+	init: function (vy) {
+		this.vy = vy
+	},
+	think: function (dt) {
+		this.dy -= this.vy * dt
+	},
+}
+var Fade = {
+	init: function (t) {
+		this.tfade = t || 1
+		this.fadetimer = 0
+	},
+	think: function (dt) {
+		this.fadetimer += dt
+		this.dead = this.fadetimer > this.tfade
+	},
+	draw: function () {
+		context.globalAlpha = clip(1 - this.fadetimer / this.tfade, 0, 1)
 	},
 }
 
 
+
+var ActionHUD = {
+	effects: [
+		// progress indicator
+		UFX.Thing()
+		.addcomp(AnchorBottomRight, settings.sx - 5, settings.sy - 5)
+		.addcomp(FillStroke, "bold 40px sans-serif", "white", "black", 2)
+		.addcomp(NoCache)
+		.addcomp({
+			think: function (dt) { this.settext("\u00A3" + gamestate.catchamount + "/" + gamestate.goal) },
+		}),
+		// countdown indicator
+		UFX.Thing()
+		.addcomp(AnchorBottomRight, settings.sx * 0.8, settings.sy - 5)
+		.addcomp(FillStroke, "bold 40px sans-serif", "rgb(32,255,32)", "black", 2)
+		.addcomp(NoCache)
+		.addcomp({
+			think: function (dt) {
+				var t = gamestate.time
+				this.settext(t > 0 ? "time:" + Math.floor(t) : "")
+				this.fstyle = t > 10 ? "rgb(32,255,32)" : "rgb(255,32,32)"
+			},
+		}),
+		// height indicator
+		UFX.Thing()
+		.addcomp(AnchorBottomLeft, 10, settings.sy - 5)
+		.addcomp(FillStroke, "bold 40px sans-serif", "blue", "black", 2)
+		.addcomp(NoCache)
+		.addcomp({
+			think: function (dt) {
+				var h = Math.floor(You.y / 25)
+				this.settext("" + h + "ft")
+			},
+		}),
+		// combo indicator
+		UFX.Thing()
+		.addcomp(AnchorBottomCenter, settings.sx * 0.35, settings.sy - 5)
+		.addcomp(FillStroke, "bold 40px sans-serif", "yellow", "black", 2)
+		.addcomp(NoCache)
+		.addcomp({
+			think: function (dt) {
+				var c = gamestate.combocount
+				this.settext(c > 1 ? "" + c + "xCombo" : "")
+			},
+		}),
+		
+	],
+	levelinit: function () {
+	},
+	think: function (dt) {
+		this.effects.forEach(function (e) { e.think(dt) })
+		this.effects = this.effects.filter(function (e) { return !e.dead })
+	},
+	draw: function () {
+		function draw(e) { context.save() ; e.draw() ; context.restore() }
+		this.effects.forEach(draw)
+	},
+
+	addcombocasheffect: function (c) {
+		this.effects.push(new CashEffect(c, settings.sx * 0.35, settings.sy - 20))
+	},
+}
+
+function CashEffect(amount, x, y) {
+	console.log(amount)
+	this.settext("\u00A3" + amount)
+	this.dx = x
+	this.dy = y
+}
+CashEffect.prototype = UFX.Thing()
+	.addcomp(AnchorCenter)
+	.addcomp(Float, 70)
+	.addcomp(Fade, 0.5)
+	.addcomp(FillStroke, "bold 40px sans-serif", "white", "black", 2)
+	.addcomp(NoCache)
 
