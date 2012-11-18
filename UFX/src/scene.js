8d6bb6582b7d9ed4112d6d473e8a0c59ce429be9
Christopher Night <cosmologicon@gmail.com>
2012-11-18 17:15:01 -0500

    UFX.scene - enqueue stack updates

--- UFX/src/scene.js
+++ UFX/src/scene.js
@@ -14,29 +14,43 @@ UFX.scene.init = function (maxups, minups) {
     UFX.scene.jrecord = 0
 }
 
+UFX.scene._actionq = []
 UFX.scene.top = function () {
     var n = UFX.scene._stack.length
     return n ? UFX.scene._stack[n-1] : null
 }
-UFX.scene.push = function (c) {
+UFX.scene.ipush = function (c) {
     var old = UFX.scene.top()
     if (old) old.suspend()
     UFX.scene._stack.push(c)
     c.start()
 }
-UFX.scene.pop = function () {
+UFX.scene.ipop = function () {
     var c = UFX.scene._stack.pop()
     c.stop()
     var n = UFX.scene.top()
     if (n) n.resume()
     return c
 }
-UFX.scene.swap = function (c) {
+UFX.scene.iswap = function (c) {
 	var c0 = UFX.scene._stack.pop()
 	c0.stop()
 	UFX.scene._stack.push(c)
 	c.start()
 	return c0
+}
+UFX.scene.push = function (c) { UFX.scene._actionq.push(["push", c]) }
+UFX.scene.pop = function (c) { UFX.scene._actionq.push(["pop"]) }
+UFX.scene.swap = function (c) { UFX.scene._actionq.push(["swap", c]) }
+UFX.scene._resolveq = function () {
+    for (var j = 0 ; j < UFX.scene._actionq.length ; ++j) {
+        switch (UFX.scene._actionq[j][0]) {
+            case "push": UFX.scene.ipush(UFX.scene._actionq[j][1]) ; break
+            case "pop": UFX.scene.ipop() ; break
+            case "swap": UFX.scene.iswap(UFX.scene._actionq[j][1]) ; break
+        }
+    }
+    UFX.scene._actionq = []
 }
 UFX.scene.think = function (dt) {
     var c = UFX.scene.top()
@@ -62,6 +76,7 @@ UFX.scene.draw = function (f) {
     if (UFX.scene._lastthinker) {
         UFX.scene._lastthinker.draw(f)
     }
+    UFX.scene._resolveq()
 }
 
 // Use this as a prototype for your own scenes
