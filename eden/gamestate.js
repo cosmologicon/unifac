08f4304f306c604f582002ecd1428cfbb79f7a84
Christopher Night <cosmologicon@gmail.com>
2012-12-15 14:11:18 -0500

    Eden Gem-seeking behavior implemented

--- eden/gamestate.js
+++ eden/gamestate.js
@@ -4,18 +4,34 @@ var gamestate = {
 		blobs = [new Blob(500, 100)]
 		platforms = [
 			new SinglePlatform(300, 300, 700, 300),
-			new SinglePlatform(200, 200, 220, 500),
-			new SinglePlatform(220, 500, 780, 500),
-			new SinglePlatform(780, 500, 800, 200),
+			new MultiPlatform(200, 200, 220, 500, 780, 500, 800, 200),
 		]
-		scenery = [
+		this.gems = [
+			new Gem(500, 150),
+		]
+		this.turners = [
 			new Turner(350, 300, true, platforms[0]),
 			new Turner(650, 300, false, platforms[0]),
 		]
+		scenery = []
+		scenery.push.apply(scenery, this.gems)
+		scenery.push.apply(scenery, this.turners)
+
 		this.sincounts = {
 			defy: 10,
 			want: 10,
 		}
+	},
+	nearestgem: function (x, y) {
+		var nearest = null, d2min
+		this.gems.forEach(function (gem) {
+			var d2 = (gem.x - x) * (gem.x - x) + (gem.y - y) * (gem.y - y)
+			if (!nearest || d2 < d2min) {
+				d2min = d2
+				nearest = gem
+			}
+		})
+		return nearest
 	},
 }
 
