cb88c6614686ec9a463ddfacf1427190fcf65661
Christopher Night <cosmologicon@gmail.com>
2012-12-16 13:57:41 -0500

    Devil's Handiwork Much nicer looking platforms

--- eden/platforms.js
+++ eden/platforms.js
@@ -144,10 +144,75 @@ MultiPlatform.prototype = {
 		return this.nearest(x, y).canhold()
 	},
 	draw: function (con) {
+		if (!this.drawspec) {
+			this.drawspec = []
+			var D = 0, ps = this.platforms
+			ps.forEach(function (p) { D += p.d })
+			function palong(p, x, y) {
+				return [p.x + x * p.ix - y * p.iy, p.y + x * p.iy + y * p.ix]
+			}
+			var ts = [0]
+			for (var j = 0 ; j < ps.length - 1 ; ++j) {
+				var dA = (ps[j+1].A - ps[j].A) / 2
+				ts.push(Math.tan((ps[j+1].A - ps[j].A) / 2))
+			}
+			ts.push(0)
+			function along(x, y) {
+				var j = 0
+				while (j < ps.length - 1 && x > ps[j].d) x -= ps[j++].d
+				x += y * (ts[j] - (ts[j+1] + ts[j]) * x / ps[j].d)
+				return palong(ps[j], x, y)
+			}
+			var depths = { 0: 0, 1: 0 }, n = 2 + Math.floor(D / 200)
+			for (var j = 0 ; j < n ; ++j) {
+				var x = (j + 1) / (n + 2.)
+				var y = UFX.random(0.2, 1) * Math.min(x*D/200,1,(1-x)*D/200)
+				depths[x] = y
+			}
+			function depthat(xa) {
+				var less = [], more = []
+				for (var x in depths) {
+					if (+x < xa) { less.push(+x) }
+					else { more.push(+x) }
+				}
+				if (!less.length || !more.length) return 0
+				var x0 = Math.max.apply(Math, less), x1 = Math.min.apply(Math, more)
+				if (x1 - x0 < 0.0001) return depths[x0]
+				return ((xa - x0) * depths[x1] + (x1 - xa) * depths[x0]) / (x1 - x0)
+			}
+
+			for (var j = 0 ; j < 2*D ; ++j) {
+				var xf = UFX.random(), yf = UFX.random()
+				var x = xf*D, y = 10 + 100 * depthat(xf) * yf, r = UFX.random(12, 24) * (1 - 0.2*j*j/D/D)
+				var red = Math.floor(UFX.random(80, 120) * (1 - 0.5 * yf))
+				var green = Math.floor(UFX.random(40, 60) * (1 - 0.5 * yf))
+				var color = "rgb(" + red + "," + green + ",0)"
+				this.drawspec.push(["b o", along(x, y), r, "fs", color, "f"])
+			}
+
+			for (var j = 0 ; j < D ; ++j) {
+				var x = UFX.random(-15,D+15), y = UFX.random() < 0.1 ? UFX.random(-15,25) : UFX.random(-10,15)
+				var r = UFX.random(6, 12) * (1 - 0.8*j*j/D/D)
+				var red = Math.floor(UFX.random(10, 20))
+				var green = Math.floor(UFX.random(140, 240))
+				var color = "rgb(" + red + "," + green + ",0)"
+				this.drawspec.push(["b o", along(x, y), r, "fs", color, "f"])
+			}
+	/*
+			for (var j = 0 ; j < this.d ; ++j) {
+				var x = UFX.random(this.d), y = UFX.random(-5, 5), r = UFX.random(4, 8)
+				if (UFX.random() < 0.5) {
+					y = UFX.random(-5, 15)
+					if (UFX.random() < 0.2) {
+						y = UFX.random(-5, 22)
+					}
+				}
+				var color = "rgb(" + UFX.random.rand(10, 20) + "," + UFX.random.rand(140,240) + ",0)"
+				this.drawspec.push(["b o", x*this.ix - y * this.iy, x*this.iy + y*this.ix, r, "fs", color, "f"])
+			}*/
+		}
 		con = con || context
-		this.platforms.forEach(function (platform) {
-			con.save() ; platform.draw(con) ; con.restore()
-		})
+		UFX.draw(con, this.drawspec)
 	},
 }
 
