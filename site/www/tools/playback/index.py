a8a0389f1a1529f6f325bafc6b9e19fce210f5c5
Christopher Night <cosmologicon@gmail.com>
2013-01-02 08:24:24 -0500

    Website utility for creating game in playback DB

--- site/www/tools/playback/index.py
+++ site/www/tools/playback/index.py
@@ -15,10 +15,28 @@ html = """Content-type: text/html
 <title>UFX.playback portal</title>
 <h1>UFX.playback portal</h1>"""
 
-if act == "":
-	print(html)
-	print("<p>Games:\n<ul>")
+print(html)
+
+if act == "creategame":
+	gamename = form["gamename"]
+	if user:
+		if playbackdb.creategame(gamename, user):
+			print("<p>Created game %s" % gamename)
+		else:
+			print("<p>Unable to create game %s" % gamename)
+	else:
+		print("<p>You must be signed in to add a game to the database")
+if act == "" or act == "creategame":
+	print("<p>Signed in as: %s" % user)
+	print("<p>Games visible to you:\n<ul>")
 	for gamename in playbackdb.gamenames(user):
 		print("<li>%s" % gamename)
 	print("</ul>")
 
+print("""<p>
+<form name="creategame" action="http://universefactory.net/tools/playback/" method="post">
+<input type="hidden" name="act" value="creategame">
+<p>Add a game to the database: <input type="text" name="gamename">
+<p><input type="submit" value="Submit">
+</form>
+""")
