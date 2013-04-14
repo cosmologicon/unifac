ea5f744a541d969e7cc40ec8726c7909aa2ffeaa
Christopher Night <cosmologicon@gmail.com>
2013-04-14 06:35:32 -0400

    PyWeek 16 Active devices and fog

--- pyweek16/src/serverstate.py
+++ pyweek16/src/serverstate.py
@@ -1,14 +1,27 @@
 import logging
-import grid
+import grid, util
 
 log = logging.getLogger(__name__)
 
 gridstate = grid.Grid()
+gridstate.fillsector(0, 0)
+gridstate.setdevice(2, 2, "eye")
+gridstate.gettile(2, 2).active = True
+for sx, sy in util.horizonsectors(gridstate.gettile(2, 2)):
+	gridstate.fillsector(sx, sy)
+	util.solvefog(gridstate, sx, sy)
+gridstate.gettile(20, 60).device = "eye"
+gridstate.gettile(20, 60).active = True
+for sx, sy in util.horizonsectors(gridstate.gettile(20, 60)):
+	gridstate.fillsector(sx, sy)
+	util.solvefog(gridstate, sx, sy)
+
+
 
 users = set()
 activeusers = set()
 
 def rotate(who, (x, y), dA):
-	gridstate.gettile(x, y).rotate(dA)
+	gridstate.gettile(x, y).rotate(gridstate, dA)
 
 
--- pyweek16/src/vista.py
+++ pyweek16/src/vista.py
@@ -26,29 +26,45 @@ def drag(dx, dy):
 	global camerax0, cameray0
 	camerax0 -= dx
 	cameray0 -= dy
-
+def visibletiles():
+	xmin, ymin = screentotile((0, 0))
+	xmax, ymax = screentotile((settings.screenx, settings.screeny))
+	for x in range(xmin, xmax+1):
+		for y in range(ymin, ymax+1):
+			yield x, y
 
 tilecache = {}
-def gettile(tile, state):
-	key = tuple(tile.colors), state
+def gettile(tile):
+	key = tuple(tile.colors), tile.device, tile.fog, tile.active
 	if key in tilecache:
 		return tilecache[key]
 	img = pygame.Surface((40, 40)).convert_alpha()
 	img.fill((0,0,0,0))
-	bcolor = (200, 200, 200) if state else (100, 100, 100)
+	bcolor = (200, 200, 200) if tile.active else (100, 100, 100)
 	img.fill(bcolor, (1,1,38,38))
 	rs = (10,1,20,5), (35,10,5,20), (10,35,20,5), (1,10,5,20)
 	for rect, colorcode in zip(rs, tile.colors):
 		img.fill(settings.colors[colorcode], rect)
+	if tile.device:
+		pygame.draw.circle(img, (255, 0, 255), (20, 20), 10)
+	if tile.fog == settings.penumbra:
+		img.fill((0,0,0))
+	else:
+		mask = pygame.Surface((40, 40)).convert_alpha()
+		mask.fill((0,0,0,140))
+		for _ in range(tile.fog):
+			img.blit(mask, (0, 0))
 	tilecache[key] = img
 	return img
 
 def draw():
 	screen.fill((0,0,0))
-	for x in range(10):
-		for y in range(10):
-			img = gettile(clientstate.gridstate.gettile(x, y), clientstate.gridstate.tilestate(x, y))
-			screen.blit(img, worldtoscreen((x, y)))
+	for x, y in visibletiles():
+		tile = clientstate.gridstate.gettile(x, y)
+		if not tile:
+			continue
+		img = gettile(tile)
+		screen.blit(img, worldtoscreen((x, y)))
 	pygame.display.flip()
 
 def screenshot():
