6c6cf430378c31828be21e7c1ce329c5ea0ce5a7
Christopher Night <cosmologicon@gmail.com>
2013-06-08 19:02:00 -0400

    Nightfall Doubletrip level and a couple tutorials

--- nfall/settings.js
+++ nfall/settings.js
@@ -10,3 +10,8 @@ var settings = {
 	
 }
 
+function clip(x,a,b){return b===undefined?x>a?a:x<-a?-a:x:x>b?b:x<a?a:x}
+function rmod(x,z){return(x%z+z)%z}
+function zmod(x,z){return((x+z/2)%z+z)%z-z/2}
+var tau = 6.283185307179586
+
