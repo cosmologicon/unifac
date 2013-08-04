5a234e8ea6a2f0eda881dcf2089a2b633fcd53a3
Christopher Night <cosmologicon@gmail.com>
2013-08-03 20:18:40 -0400

    Robot Underground Maps loaded from json rather than bmps

--- robot-underground/worldgen.js
+++ robot-underground/worldgen.js
@@ -1,30 +1,9 @@
-
-/*
-function makeTown():
-	var map = new DungeonGrid(100), r
-	;(new Room([0, 0], [5, 5])).addToDungeon(map)
-	;(new Room([6, 0], [3, 5])).addToDungeon(map)
-	;[[5,2], [5,3], [5,4], [9,3], [9,4], [10,3], [10,4]].forEach(function (p) {
-		map.setCell(p, 1)
-	}
-	return map
-}*/
-
-function extractImgData(img) {
-	var can = document.createElement("canvas")
-	can.width = img.width ; can.height = img.height
-	var con = can.getContext("2d")
-	con.drawImage(img, 0, 0)
-	return con.getImageData(0, 0, img.width, img.height).data
-}
-
 function loadFixedMap(name) {
 	var map = new DungeonGrid(100)
-	var img = UFX.resource.images[name]
-	var data = extractImgData(img)
-	for (var y = img.height-1, j = 0 ; y >= 0 ; --y) {
-		for (var x = 0 ; x < img.width ; ++x, j += 4) {
-			if (data[j]) map.setCell([x, y], 1)
+	var img = mapdata[name]
+	for (var y = img.h-1, j = 0 ; y >= 0 ; --y) {
+		for (var x = 0 ; x < img.w ; ++x, ++j) {
+			if (img.data[j]) map.setCell([x, y], 1)
 		}
 	}
 	return map
