53ca7a1a57dbb11f845bcc980730db2fb259bc25
Christopher Night <cosmologicon@gmail.com>
2014-01-26 18:17:06 -0500

    Obb debugHUD timers added

--- obb/src/scenes.js
+++ obb/src/scenes.js
@@ -12,6 +12,7 @@ function istate() {
 // see them on mobile as well.
 var debugHUD = {
 	alerts: [],
+	timers: {},
 	alert: function (text) {
 		if (!settings.DEBUG) return
 		this.alerts.push([text, Date.now()])
@@ -26,7 +27,17 @@ var debugHUD = {
 		// FPS counter
 		var fpsstr = UFX.ticker.getrates().split(" ")[0]
 		text.draw(fpsstr, 0.2*h, 0.3*h, { fontsize: h })
-		this.alerts.forEach(function (alert, j, alerts) {
+		var alerts = this.alerts.splice()
+		for (var tname in this.timers) {
+			var timer = this.timers[tname]
+			if (!timer.length) continue
+			var avg = timer.reduce(function (a, b) { return a + b }) / timer.length
+			alerts.push([
+				tname + ": " + avg.toPrecision(3) + "ms",
+				Date.now(),
+			])
+		}
+		alerts.forEach(function (alert, j, alerts) {
 			var y = canvas.height - 10 - 24 * (alerts.length - j - 1)
 			var alpha = clamp(4 - (Date.now() - alert[1]) / 500, 0, 1)
 			text.draw(alert[0], 0.2*h, (0.3 + 1.2 * (alerts.length - j)) * h, {
@@ -37,6 +48,17 @@ var debugHUD = {
 		this.alerts = this.alerts.filter(
 			function (alert) { return Date.now() - alert[1] < 2000 }
 		)
+	},
+	starttimer: function (tname) {
+		if (!settings.DEBUG) return
+		if (!this.timers[tname]) this.timers[tname] = []
+		this.timers[tname].t0 = Date.now()
+	},
+	stoptimer: function (tname) {
+		if (!settings.DEBUG) return
+		var timer = this.timers[tname]
+		timer.push(Date.now() - timer.t0)
+		if (timer.length > 100) timer.shift()
 	},
 }
 
@@ -56,6 +78,7 @@ UFX.scenes.play = {
 	},
 	think: function (dt, input) {
 		debugHUD.think(dt)
+		debugHUD.starttimer("control")
 		var cevents = controlstate.think(dt, input)
 		cevents.forEach(function (cevent) {
 			var fname = "handle" + cevent.type
@@ -69,6 +92,7 @@ UFX.scenes.play = {
 				}
 			}
 		})
+		debugHUD.stoptimer("control")
 
 		state.think(dt)
 	},
