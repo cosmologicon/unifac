ea52bb821175bc32390c3e53101c6c5c81839052
Christopher Night <cosmologicon@gmail.com>
2014-04-26 15:09:09 -0400

    Ludum Dare 29 Building platforms

--- ld29/play.js
+++ ld29/play.js
@@ -1,15 +1,49 @@
 UFX.scenes.play = {
 	start: function () {
 		state.init()
+		this.mode = "play"
+		this.buildoff = [0, 0]
+		this.vplatform = new VirtualPlatform(0, 0, 1)
 	},
 	thinkargs: function (dt) {
 		return [dt, UFX.key.state()]
 	},
 	think: function (dt, kstate) {
-		if (kstate.down.up) {
-			state.you.leap()
+		if (this.mode == "play") {
+			if (kstate.down.up) {
+				state.you.leap()
+			}
+			if (state.you.parent && kstate.down.down) {
+				state.you.drop()
+			}
+			state.you.hmove((kstate.pressed.right ? 1 : 0) - (kstate.pressed.left ? 1 : 0))
+			if (kstate.down.space) {
+				this.mode = "build"
+				this.vplatform.x = Math.floor(state.you.x) + this.buildoff[0]
+				this.vplatform.y = Math.floor(state.you.y) + this.buildoff[1]
+			}
+		} else if (this.mode == "build") {
+			if (kstate.down.left) this.buildoff[0] -= 1
+			if (kstate.down.right) this.buildoff[0] += 1
+			if (kstate.down.up) this.buildoff[1] += 1
+			if (kstate.down.down) this.buildoff[1] -= 1
+			this.vplatform.x = Math.floor(state.you.x) + this.buildoff[0]
+			this.vplatform.y = Math.floor(state.you.y) + this.buildoff[1]
+			if (!kstate.pressed.space) {
+				state.platforms.push(new Platform(this.vplatform.x, this.vplatform.y, this.vplatform.dx))
+				this.mode = "play"
+			}
 		}
+
+
 		state.you.think(dt)
+		if (!state.you.parent && state.you.vy < 0) {
+			state.platforms.forEach(function (platform) {
+				if (platform.catches(state.you)) state.you.land(platform)
+			})
+		}
+		camera.x0 = state.you.x
+		camera.y0 = state.you.y
 	},
 	draw: function () {
 		UFX.draw("fs black f0")
@@ -20,7 +54,11 @@ UFX.scenes.play = {
 			obj.draw()
 			context.restore()
 		}
+		state.platforms.forEach(draw)
 		draw(state.you)
+		if (this.mode == "build") {
+			draw(this.vplatform)
+		}
 		context.restore()
 	},
 }
