6ea7519fad2c6e37cfe2a7028dcb02ce82e89d11
Christopher Night <cosmologicon@gmail.com>
2014-04-26 17:40:17 -0400

    Ludum Dare 29 Rudimentary level editor capabilities

--- ld29/settings.js
+++ ld29/settings.js
@@ -5,5 +5,12 @@ var settings = {
 	aydown: 40,
 	vx: 6,
 }
-
+var qobj = {}
+var qstring = window.location.search.slice(1)
+qstring.split("&").forEach(function (qstr) {
+	var a = qstr.split("=")
+	if (a.length == 1) qobj[a[0]] = true
+	if (a.length == 2) qobj[a[0]] = JSON.parse(decodeURIComponent(a[1]))
+})
+var DEBUG = qobj.DEBUG
 
