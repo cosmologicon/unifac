2e004035cfc2a39d95b252f7e4c2cb75749e8ce5
Christopher Night <cosmologicon@gmail.com>
2014-08-23 16:53:35 -0400

    LD30 selectable controls

--- ld30/control.js
+++ ld30/control.js
@@ -1,8 +1,74 @@
 
 var control = {
+	init: function () {
+		this.buttons = {
+			bridge: {
+				main: true,
+				offset: 0,
+				path: "b o 0.3 0.5 0.1 o 0.6 0.5 0.1 f",
+			},
+			relay: {
+				main: true,
+				offset: 1,
+				path: "b o 0.5 0.5 0.25 lw 0.1 s",
+			},
+			settings: {
+				main: false,
+				offset: 0,
+				path: "b m 0.2 0.2 l 0.8 0.2 lw 0.1 s",
+			},
+		}
+		this.selectedbutton = null
+	},
 	// closest point to p that's directly horizontal or vertical to p0
 	nearest: function (p0, p) {
 		return Math.abs(p0[0] - p[0]) > Math.abs(p0[1] - p[1]) ? [p[0], p0[1]] : [p0[0], p[1]]
+	},
+	
+	think: function (dt) {
+		var sx = canvas.width, sy = canvas.height
+		var bsize = Math.floor(0.2 * Math.min(sx, sy))
+		for (var bname in this.buttons) {
+			var button = this.buttons[bname]
+			button.w = button.h = bsize
+			if (sx < sy) {
+				button.x = button.offset * bsize
+				button.y = button.main ? 0 : sy - bsize
+			} else {
+				button.x = button.main ? 0 : sx - bsize
+				button.y = button.offset * bsize
+			}
+		}
+	},
+
+	buttonat: function (pos) {
+		for (var bname in this.buttons) {
+			var b = this.buttons[bname]
+			if (b.x < pos[0] && pos[0] < b.x + b.w && b.y < pos[1] && pos[1] < b.y + b.h) {
+				return bname
+			}
+		}
+		return null
+	},
+	
+	selectbutton: function (bname) {
+		this.selectedbutton = this.selectedbutton == bname ? null : bname
+	},
+	
+	draw: function () {
+		for (var bname in this.buttons) {
+			var button = this.buttons[bname]
+			UFX.draw(
+				"[ t", button.x, button.y, "z", button.w, button.h,
+				"b rr 0.05 0.05 0.9 0.9 0.15",
+				"fs rgba(0,255,255," + (bname == this.selectedbutton ? 0.4 : 0.1) + ")",
+				"ss rgba(0,255,255," + (bname == this.selectedbutton ? 0.8 : 0.4) + ")",
+				"lw 0.04 f s",
+				button.path,
+				"]"
+			)
+		}
+	
 	},
 }
 
--- ld30/init.js
+++ ld30/init.js
@@ -1,3 +1,7 @@
+
+function clamp(x,a,b){return b===undefined?x>a?a:x<-a?-a:x:x>b?b:x<a?a:x}
+var tau = 6.283185307179586
+
 var canvas = document.getElementById("canvas")
 var context = canvas.getContext("2d")
 UFX.draw.setcontext(context)
@@ -7,27 +11,42 @@ UFX.draw("fs blue f0")
 
 UFX.scene.init({ minups: 5, maxups: 120 })
 UFX.mouse.init(canvas)
+UFX.touch.active = false
+canvas.ontouchstart = function (event) {
+	UFX.touch.active = true
+	UFX.touch.init(canvas)
+	UFX.mouse.active = false
+}
 
 background.init()
+control.init()
 
 UFX.scene.push({
 	thinkargs: function (dt) {
-		return [dt, UFX.mouse.pos || [0, 0]]
+		return [
+			dt, 
+			UFX.mouse.active && UFX.mouse.state(),
+			UFX.touch.active && UFX.touch.state(),
+		]
 	},
-	think: function (dt, mpos) {
-		this.bpos = control.nearest([2, 1], view.togame(mpos[0], mpos[1]))
+	think: function (dt, mstate, tstate) {
 		view.think(dt)
 		background.think(dt)
+		control.think(dt)
+		if (mstate && mstate.left.down) {
+			var b = control.buttonat(mstate.left.down)
+			if (b) control.selectbutton(b)
+		}
 	},
 	draw: function () {
 		background.draw()
 		UFX.draw("[")
 		view.transform()
-		UFX.draw("b m 2 1 l", this.bpos, "ss yellow lw 0.04 s")
 		UFX.draw("b o 0 0 0.2 fs blue f")
 		UFX.draw("b o 2 1 0.2 fs green f")
 		
 		UFX.draw("]")
+		control.draw()
 	},
 })
 
