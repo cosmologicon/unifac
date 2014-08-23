c1daf09ccd691f6f4218b4b92f0e17553297139f
Christopher Night <cosmologicon@gmail.com>
2014-08-23 18:28:09 -0400

    LD30 Draggable viewport

--- ld30/control.js
+++ ld30/control.js
@@ -19,10 +19,15 @@ var control = {
 			},
 		}
 		this.selectedbutton = null
+		this.dragstart = null
 	},
 	// closest point to p that's directly horizontal or vertical to p0
 	nearest: function (p0, p) {
 		return Math.abs(p0[0] - p[0]) > Math.abs(p0[1] - p[1]) ? [p[0], p0[1]] : [p0[0], p[1]]
+	},
+	// Start of a potential mouse drag event or swipe event
+	registerstart: function (pos) {
+		this.dragstart = pos
 	},
 	
 	think: function (dt) {
