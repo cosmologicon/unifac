9bc0a8980df93a17b99b122a865a6b1f6ac60709
Christopher Night <cosmologicon@gmail.com>
2014-10-04 21:00:19 -0400

    PyWeek 19 Couple basic modules in place

---
+++ pyweek19/src/main.py
@@ -0,0 +1,26 @@
+import settings, scene, vista, gamescene
+import pygame
+
+pygame.init()
+vista.init()
+pygame.display.set_caption(settings.gamename)
+scene.push(gamescene)
+
+clock = pygame.time.Clock()
+while True:
+	scene0 = scene.top()
+	if not scene0:
+		break
+	dt = clock.tick(settings.maxfps) * 0.001
+	events = pygame.event.get()
+	if settings.DEBUG:
+		pygame.display.set_caption("%s | %.1ffps" % (settings.gamename, clock.get_fps()))
+	if any(event.type == pygame.QUIT for event in events):
+		break
+	scene0.think(dt, events)
+	scene0.draw()
+	vista.flip()
+
+pygame.quit()
+
+
