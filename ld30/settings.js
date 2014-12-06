7d265b563498cd75bd4e32338fd72d98ff47f744
Christopher Night <cosmologicon@gmail.com>
2014-12-05 20:09:35 -0500

    Ludum Dare 30 Final tweaks, I guess

--- ld30/settings.js
+++ ld30/settings.js
@@ -3,8 +3,8 @@ var settings = {
 	savename: "LD30save",
 	DEBUG: window.location.href.indexOf("-dev") > -1,
 	EDITOR: window.location.href.indexOf("EDITOR") > -1,
-	rescolors: ["rgb(0,0,255)", "rgb(200,100,0)", "red", "white"],
-	resfades: ["rgba(0,0,255,0)", "rgba(200,100,0,0)", "red", "white"],
+	rescolors: ["rgb(0,0,255)", "rgb(200,100,0)", "rgb(255,0,0)", "white"],
+	resfades: ["rgba(0,0,255,0)", "rgba(200,100,0,0)", "rgba(255,0,0,0)", "white"],
 	bloidcost: 4,
 }
 if (window.location.href.indexOf("RESET") > -1) {
