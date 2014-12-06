ca46587c544298f6a7ce841da281a1c870473299
Christopher Night <cosmologicon@gmail.com>
2014-12-06 12:22:48 -0500

    Ludum Dare 31 Basic platform mechanics

--- ld31/settings.js
+++ ld31/settings.js
@@ -2,6 +2,8 @@ var settings = {
 	gamename: "LD31",
 	savename: "LD31save",
 	DEBUG: window.location.href.indexOf("DEBUG") > -1,
+	
+	g: 10,
 }
 if (window.location.href.indexOf("RESET") > -1) {
 	delete window.localStorage[settings.savename]
